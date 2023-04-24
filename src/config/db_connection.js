ed6d01d8d4fdff2ff06f3c8a4e38afa77a292549
Lorena Tazzioli <cltazzioli@gmail.com>
2023-04-24 00:38:30 -0300

    movi y reestruture la organizacion

---
+++ src/config/db_connection.js
@@ -0,0 +1,10 @@
+import * as mongoose from "mongoose"
+
+export const dbConnection = () => {
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((error) => console.error(error));
+}
+
+
