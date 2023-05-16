184ceae63eb9189eb272a301a209a45b1331813a
Lorena Tazzioli <cltazzioli@gmail.com>
2023-05-15 23:51:10 -0300

    creación de errorHandler

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import express from "express";
 import { port } from "./src/config/index.js";
 import { dbConnection } from "./src/config/db_connection.js";
 import router from "./src/router.js"
+import errorHandler from "./src/middleware.js";
 
 const app = express();
 
@@ -10,6 +11,7 @@ const app = express();
 dbConnection();
 
 app.use(express.json());
+app.use(errorHandler);
 app.use("/", router);
 
 // Runing the server
