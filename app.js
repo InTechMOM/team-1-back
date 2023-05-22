ec561c9ed519155c2a0967a0b8408a9afa6ebd03
Lorena Tazzioli <cltazzioli@gmail.com>
2023-05-22 20:31:53 -0300

    deshabilitacion de cors

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import cors from "cors";
 import express from "express";
 import swaggerUi from "swagger-ui-express";
 import { port } from "./src/config/index.js";
@@ -10,6 +11,7 @@ const app = express();
 // Connect with mongoDB
 dbConnection();
 
+app.use(cors());
 app.use(express.json());
 app.use("/", router);
 
